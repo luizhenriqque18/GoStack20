a1b4710fbef653c81a7f9da070c8e4c0e50f8510
Luiz Henrique <luizhenriqque15@gmail.com>
2020-02-11 10:27:54 -0400

    Validação de entrada

--- nodejs/modulo02/goBarber/src/app/controller/UserController.js
+++ nodejs/modulo02/goBarber/src/app/controller/UserController.js
@@ -1,7 +1,26 @@
+/* eslint-disable prettier/prettier */
+const yup = require('yup');
+
 const User = require('../models/User');
 
 class UserController {
     async store(req, res) {
+        const schema = yup.object().shape({
+            name: yup.string().required(),
+            email: yup
+                .string()
+                .email()
+                .required(),
+            password: yup
+                .string()
+                .min(6)
+                .required(),
+        });
+
+        if (!(await schema.isValid(req.body))) {
+            return res.status(400).json('Validation error');
+        }
+
         const userExists = await User.findOne({
             where: { email: req.body.email },
         });
@@ -16,10 +35,31 @@ class UserController {
     }
 
     async update(req, res) {
-        const { email, OldPassword } = req.body;
+        const schema = yup.object().shape({
+            name: yup.string(),
+            email: yup.string().email(),
+            oldpassword: yup.string().min(6),
+            password: yup
+                .string()
+                .min(6)
+                .when('oldpassword', (oldpassword, field) =>
+                    oldpassword ? field.required() : field
+                ),
+            confirmPassword: yup
+                .string()
+                .when('password', (password, field) =>
+                    password ? field.required().oneOf([yup.ref('password')]) : field
+                ),
+        });
+
+        if(!( await schema.isValid(req.body))){
+            return res.status(400).json('Validation error');
+        }
+
+        const { email, oldPassword } = req.body;
 
         const user = await User.findByPk(req.userId);
-
+        
         if (email && email !== user.email) {
             const userExists = await User.findOne({ where: { email } });
 
@@ -28,7 +68,7 @@ class UserController {
             }
         }
 
-        if (OldPassword && !(await user.checkPassword(OldPassword))) {
+        if (oldPassword && !(await user.checkPassword(oldPassword))) {
             return res.status(401).json({ error: 'Password does not match' });
         }
 
